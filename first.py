eb31eae229b655476372c8e2c7afb8ab3b15a0d7
IevaLaicane <75501250+IevaLaicane@users.noreply.github.com>
2021-05-08 12:55:39 +0200

    Delete first.py

--- first.py
+++
@@ -1,7 +0,0 @@
-print("Calculate your gross salary for this week!")
-user = input("Please enter your name: ")
-hours = (float(input(user + ", please enter hours you worked this week: ")))
-
-payrate = (float(input(user + ", please enter your payrate per hour: €")))
-salary = hours * payrate 
-print(user + ", your gross salary for this week is €", salary)
